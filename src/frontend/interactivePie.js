513e219c946c918e63649d0bfd42c4e236864143
Peter Rhodes <rhodespeter@hotmail.co.uk>
2018-12-19 16:48:30 +0000

    Get transitions working! (not pretty and no labels)

--- src/frontend/interactivePie.js
+++ src/frontend/interactivePie.js
@@ -7,10 +7,6 @@ const colours = {
   ni: '#FFD700',
 };
 
-const arc = (d3, radius) => d3.arc()
-  .innerRadius(0)
-  .outerRadius(radius);
-
 const displaySVG = (d3, dataset) => {
   const width = 400;
   const height = 400;
@@ -20,19 +16,32 @@ const displaySVG = (d3, dataset) => {
     .attr('width', width)
     .attr('height', height)
     .append('g')
-    .attr('transform', `translate(${width / 2}, ${height / 2})`)
+    .attr('transform', `translate(${width / 2}, ${height / 2})`);
+
+  const arc = d3.arc()
+    .innerRadius(0)
+    .outerRadius(radius);
 
   const pie = d3.pie()
     .value(d => d.count)
     .sort(null);
+
+  function arcTween(a) {
+    const i = d3.interpolate(this.current, a);
+    this.current = i(0);
+    return t => arc(i(t));
+  }
 
   svg.selectAll('path')
     .data(pie(dataset))
     .enter()
     .append('g')
     .append('path')
-    .attr('d', arc(d3, radius))
+    .attr('d', arc)
     .attr('fill', d => colours[d.data.country])
+    .transition()
+    .duration(500)
+    .attrTween('d', arcTween);
 };
 
 const updateSVG = (d3, newDataset) => {
@@ -40,14 +49,27 @@ const updateSVG = (d3, newDataset) => {
   const height = 400;
   const radius = Math.min(width, height) / 2;
 
+  const arc = d3.arc()
+    .innerRadius(0)
+    .outerRadius(radius);
+
   const pie = d3.pie()
     .value(d => d.count)
     .sort(null);
 
+  function arcTween(a) {
+    const i = d3.interpolate(this.current, a);
+    this.current = i(0);
+    return t => arc(i(t));
+  }
+
   d3.selectAll('path')
     .data(pie(newDataset))
-    .attr('d', arc(d3, radius))
-    .attr('fill', d => colours[d.data.country]);
+    .attr('d', arc)
+    .attr('fill', d => colours[d.data.country])
+    .transition()
+    .duration(500)
+    .attrTween('d', arcTween);
 };
 
 const attachListenersToButtons = (d3, dataset) => {
